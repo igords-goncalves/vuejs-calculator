c1d0d81b54c154ca69a1c8be283b0546266f3e13
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-12 23:49:21 -0300

    featÇ create a calculator and reusability with components

--- src/components/Main/Calculator.vue
+++ src/components/Main/Calculator.vue
@@ -1,9 +1,25 @@
 <template>
     <div
-        class="h-[320px] w-[235px] overflow-hidden rounded-md bg-slate-800 grid grid-cols-4 grid-rows-6"
+        class="h-[320px] w-[235px] overflow-hidden rounded-md bg-slate-800 grid grid-cols-4 grid-rows-6 border border-slate-950"
     >
-        <Display />
-        <Button />
+        <Display value="0" />
+        <Button label="AC" triple />
+        <Button label="/" operation />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" operation />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" operation />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" operation />
+        <Button label="0" double />
+        <Button label="." />
+        <Button label="=" operation />
     </div>
 </template>
 
