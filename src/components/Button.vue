1a476fb832b32093e1a53b52e1a60cad6de64a4e
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-12 23:48:23 -0300

    featÇ create a component button and passing props

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,34 @@
 <template>
-<button>
-    0
-</button>
-        
-
+    <button
+        class="button bg-slate-700 border hover:bg-slate-800 border-slate-950 outline-none"
+        :class="{ double, triple, operation }"
+    >
+        {{ label }}
+    </button>
 </template>
 
 <script>
 export default {
-
-}
-
+    props: {
+        label: String,
+        operation: Boolean,
+        double: Boolean,
+        triple: Boolean,
+    },
+};
 </script>
+
+<style>
+.button.double {
+    grid-column: span 2;
+}
+.button.triple {
+    grid-column: span 3;
+}
+.button.operation {
+    background-color: #fa8231cc;
+    &:hover {
+        background-color: #bd6123;
+    }
+}
+</style>
