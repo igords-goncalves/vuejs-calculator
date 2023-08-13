add2a262a61650e2b05a1d7d74e6ed993f195b99
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-13 12:25:09 -0300

    feat: add logic for calculator

--- src/components/Main/Calculator.vue
+++ src/components/Main/Calculator.vue
@@ -2,7 +2,7 @@
     <div
         class="h-[320px] w-[235px] overflow-hidden rounded-md bg-slate-800 grid grid-cols-4 grid-rows-6 border border-slate-950"
     >
-        <Display value="0" />
+        <Display v-bind:value="displayValue" />
         <Button label="AC" triple @onClick="clearMemory" />
         <Button label="/" operation @onClick="setOperation" />
         <Button label="7" @onClick="addDigit" />
@@ -28,19 +28,68 @@ import Display from "../Display.vue";
 import Button from "../Button.vue";
 
 export default {
+    data() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            // 0 = primeiro valor, 1 = segundo valor
+            operation: null,
+            // Valores armazenados serão do tipo float pois quero fazer operações matemáticas
+            values: [0, 0],
+
+            // current = qual valor está sendo manipulado 
+            current: 0,
+        };
+    },
     components: {
         Display,
         Button,
     },
     methods: {
         clearMemory() {
-            console.log("Clear");
+            Object.assign(this.$data, this.$options.data());
+            // $options.data() retorna um objeto com os dados iniciais do componente
         },
         setOperation(operation) {
-            console.log(operation);
+            if(this.current === 0) {
+                this.operation = operation;
+                this.current = 1;
+                this.clearDisplay = true;
+            } else {
+                const equals = operation === '=';
+                // A operação de igual vai ter uma constante pois ele é diferente das outras operações
+                const currentOperation = this.operation;
+
+                try {
+                    this.values[0] = eval(`${this.values[0]} ${currentOperation} ${this.values[1]}`);
+                } catch (error) {
+                    this.$emit('onError', error)
+                }
+                this.values[1] = 0;
+
+                this.displayValue = this.values[0];
+                this.operation = equals ? null : operation;
+                this.current = equals ? 0 : 1;
+                this.clearDisplay = !equals;
+            }
         },
         addDigit(digit) {
-            console.log(digit);
+            if(digit === '.' && this.displayValue.includes('.')) {
+                return;
+            }
+            const clearDisplay = this.displayValue === '0' || this.clearDisplay;
+            const currentValue = clearDisplay ? '' : this.displayValue;
+
+            const newDisplayValue = currentValue + digit;
+            this.displayValue = newDisplayValue;
+            // this.displayValue é o estado atual do componente
+            this.clearDisplay = false;
+
+            if(digit !== '.') {
+                const index = this.current;
+                const newValue = parseFloat(newDisplayValue);
+                this.values[index] = newValue;
+            }
         },
     },
 };
