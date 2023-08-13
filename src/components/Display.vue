2fa2c0ddfe1456e07f281573d11937231eb9b8fb
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-12 23:48:52 -0300

    featÇ create a component to display the result of the operation

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,14 +1,19 @@
 <template>
-    <button>
-        0
-    </button>
-            
-    
-    </template>
-    
-    <script>
-    export default {
-    
-    }
-    
-    </script>
+    <div
+        class="display bg-slate-950 flex justify-end items-center p-5 text-2xl overflow-hidden"
+    >
+        {{ value }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["value"],
+};
+</script>
+
+<style>
+.display {
+    grid-column: span 4;
+}
+</style>
