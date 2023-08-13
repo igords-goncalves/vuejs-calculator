c2e4275513abe76257f0a248af5d9c8d9fe232a9
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-12 23:06:04 -0300

    feat: create calculator component

---
+++ src/components/Main/Calculator.vue
@@ -0,0 +1,20 @@
+<template>
+    <div
+        class="h-[320px] w-[235px] overflow-hidden rounded-md bg-slate-800 grid grid-cols-4 grid-rows-6"
+    >
+        <Display />
+        <Button />
+    </div>
+</template>
+
+<script>
+import Display from "../Display.vue";
+import Button from "../Button.vue";
+
+export default {
+    components: {
+        Display,
+        Button,
+    },
+};
+</script>
