5cf77649882a2c5a930935473bc1ae8f0fa752ff
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-13 00:11:29 -0300

    feat: create methods and add to buttons

--- src/components/Main/Calculator.vue
+++ src/components/Main/Calculator.vue
@@ -3,23 +3,23 @@
         class="h-[320px] w-[235px] overflow-hidden rounded-md bg-slate-800 grid grid-cols-4 grid-rows-6 border border-slate-950"
     >
         <Display value="0" />
-        <Button label="AC" triple />
-        <Button label="/" operation />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" operation />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" operation />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" operation />
-        <Button label="0" double />
-        <Button label="." />
-        <Button label="=" operation />
+        <Button label="AC" triple @onClick="clearMemory" />
+        <Button label="/" operation @onClick="setOperation" />
+        <Button label="7" @onClick="addDigit" />
+        <Button label="8" @onClick="addDigit" />
+        <Button label="9" @onClick="addDigit" />
+        <Button label="*" operation @onClick="setOperation"/>
+        <Button label="4" @onClick="addDigit" />
+        <Button label="5" @onClick="addDigit" />
+        <Button label="6" @onClick="addDigit" />
+        <Button label="-" operation @onClick="setOperation"/>
+        <Button label="1" @onClick="addDigit" />
+        <Button label="2" @onClick="addDigit" />
+        <Button label="3" @onClick="addDigit" />
+        <Button label="+" operation @onClick="setOperation"/>
+        <Button label="0" @onClick="addDigit" double />
+        <Button label="." @onClick="addDigit"/>
+        <Button label="=" operation @onClick="setOperation"/>
     </div>
 </template>
 
@@ -31,6 +31,17 @@ export default {
     components: {
         Display,
         Button,
+    },
+    methods: {
+        clearMemory() {
+            console.log("Clear");
+        },
+        setOperation(operation) {
+            console.log(operation);
+        },
+        addDigit(digit) {
+            console.log(digit);
+        },
     },
 };
 </script>
